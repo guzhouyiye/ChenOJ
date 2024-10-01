173c0ae9599f1d9d12cb1cf4686e054517e3fa7a
guzhouyiye <1581241485@qq.com>
2024-10-01 11:28:04 +0800

    代码沙箱Docker实现

---
+++ chenoj-docker-code-sandbox/src/main/resources/testCode/simpleCompute/Main.java
@@ -0,0 +1,9 @@
+package com.chen.chenojcodesandbox;
+
+public class Main {
+    public static void main(String[] args) {
+        Integer a = Integer.valueOf(args[0]);
+        Integer b = Integer.valueOf(args[1]);
+        System.out.println("结果：" + (a+b));
+    }
+}
